25d7efedf5eeaeaf4deb7609a2010c219ad14d73
Kacper Adach <kacperadach@gmail.com>
2020-11-08 10:29:43 -0500

    catch errors

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from time import sleep
 
 def run():
     while 1:
-        book(FITNESS_URL, FREE_WEIGHTS_NAME, KACPER)
-        # book(POOL_URL, LAP_SWIM_NAME, ALEX)
-        sleep(10 * 60)
+        try:
+            book(FITNESS_URL, FREE_WEIGHTS_NAME, KACPER)
+            # book(POOL_URL, LAP_SWIM_NAME, ALEX)
+            sleep(10 * 60)
+        except Exception as e:
+            logger.exception('Error: {}'.format(e))
 
 if __name__ == '__main__':
     run()
